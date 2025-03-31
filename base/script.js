c2b04770f0a577a44afdfa495461d1870085f2ff
table425 <oliversyrls1@gmail.com>
2025-03-31 20:46:33 +1100

    Add files via upload

---
+++ base/script.js
@@ -0,0 +1,9 @@
+let audi = new Audio("idiot.mp3")
+
+function letthefunbegin() {
+    audi.play()
+    setInterval(function() {
+        audi.play()
+    }, 59000)
+    let newwind = window.open("table425.github.io/youareanidiotremake")
+}
